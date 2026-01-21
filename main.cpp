90ec97556b2ae53f36c4a3925f3b2c6f61ac80b3
john00003 <jetyler@ualberta.ca>
2026-01-21 06:05:44 -0700

    added depth buffer computation based on z coordinate

--- main.cpp
+++ main.cpp
@@ -135,7 +135,33 @@ void triangleWithFill(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &
 
 }
 
-std::vector<Vec2i> computeBoundingBox(std::vector<Vec2i> &vertices)
+template<typename T>
+std::vector<Vec2i> computeBoundingBox(std::vector<T> &&vertices)
+{
+    // takes as input a vector of three vertices sorted by x coordinate
+    std::vector<Vec2i> bbox;
+    int minX = vertices[0].x;
+    int maxX = vertices[2].x;
+
+    int minY = std::numeric_limits<int>::max();
+    int maxY = std::numeric_limits<int>::min();
+
+    for (auto v:vertices)
+    {
+        minY = std::min(minY, v.y);
+        maxY = std::max(maxY, v.y);
+        minX = std::min(minX, v.x);  // for more general routine that can take any number of input vertices we could do this
+        maxX = std::max(maxX, v.x);
+    }
+
+    bbox.emplace_back(minX, minY);
+    bbox.emplace_back(maxX, maxY);
+
+    return bbox;
+}
+
+template<typename T>
+std::vector<Vec2i> computeBoundingBox(std::vector<T> &vertices)
 {
     // takes as input a vector of three vertices sorted by x coordinate
     std::vector<Vec2i> bbox;
@@ -205,7 +231,8 @@ double signedTriangleArea(int ax, int ay, int bx, int by, int cx, int cy)
     //return .5 * ((ax - bx) * (ay + by) + (cx - bx) * (cy + by) + (ax - cx) * (ay + cy));
 }
 
-bool pointInTriangleBarycentricMethod(int px, int py, std::vector<Vec2i> &vertices, double totalArea)
+template <typename T>
+bool pointInTriangleBarycentricMethod(int px, int py, std::vector<T> &vertices, double totalArea)
 {
 
     double alpha = signedTriangleArea(px, py, vertices[1].x, vertices[1].y, vertices[2].x, vertices[2].y) / totalArea;
@@ -236,7 +263,7 @@ void triangleWithFillBoundingBoxMethod(int ax, int ay, int bx, int by, int cx, i
     //     std::cout << "Colors: " << (int) color.raw[0] << ", " << (int) color.raw[1] << ", " << (int) color.raw[2] << ", " << (int) color.raw[3] << std::endl;
     // }
     //sortTriangleVerticesByX(vertices);
-    std::vector<Vec2i> bbox = computeBoundingBox(vertices);
+    std::vector<Vec2i> bbox = computeBoundingBox<Vec2i>(vertices);
     //std::cout << "bbox min x: " << bbox[0].x << " " << "bbox min y: " << bbox[0].y << " " << "bbox max x: " << bbox[1].x << " " << "bbox max y: " << bbox[1].y << std::endl;
 
     for (int i=bbox[0].y; i<=bbox[1].y; i++)
@@ -304,9 +331,90 @@ void triangleWithLinearInterpolationOverBarycentric(int ax, int ay, int bx, int 
     }
 }
 
+std::expected<int, std::string> pointInTriangleBarycentricMethodWithDepthInterpolation(int px, int py, std::vector<Vec3i> &vertices, double totalArea)
+{
+    double alpha = signedTriangleArea(px, py, vertices[1].x, vertices[1].y, vertices[2].x, vertices[2].y) / totalArea;
+    double beta = signedTriangleArea(px, py, vertices[2].x, vertices[2].y, vertices[0].x, vertices[0].y) / totalArea;
+    double gamma = signedTriangleArea(px, py, vertices[0].x, vertices[0].y, vertices[1].x, vertices[1].y) / totalArea;
+
+    if (alpha<0 || beta<0 || gamma<0)
+        return std::unexpected("Outside of triangle");;
+    //{std::rand()%255, std::rand()%255, std::rand()%255, std::rand()%255}
+    //return TGAColor{alpha*colors[0], beta*colors[1], gamma*colors[2], colors[3]};
+    // int B = (int)(alpha*colors[0].b + beta*colors[1].b + gamma*colors[2].b) % 255;
+    // int G = (int)(alpha*colors[0].g + beta*colors[1].g + gamma*colors[2].g) % 255;
+    // int R = (int)(alpha*colors[0].r + beta*colors[1].r + gamma*colors[2].r) % 255;
+    // int A = (int)(alpha*colors[0].a + beta*colors[1].a + gamma*colors[2].a) % 255;
+    int thisZ = alpha*vertices[0].z + beta*vertices[1].z + gamma*vertices[2].z;
+    return thisZ;
+}
+
+void triangleWithFillPerPixelPainters(std::vector<Vec3i> &vertices, TGAImage &framebuffer, TGAImage &depthBuffer, TGAColor color)
+{
+    // Vec2i point1(ax, ay);
+    // Vec2i point2(bx, by);
+    // Vec2i point3(cx, cy);
+    // std::vector<Vec2i> vertices{point1, point2, point3};
+
+    double totalArea = signedTriangleArea(vertices[0].x, vertices[0].y, vertices[1].x, vertices[1].y, vertices[2].x, vertices[2].y);
+    //std::cout << ax << " " << ay << " " << bx << " " << by << " " << cx << " " << cy << std::endl;
+    //std::cout << "yay" << std::endl;
+    if (totalArea < 1){
+        // std::cout << "Total area is less than 1, skipping triangle" << std::endl;
+        //std::cout << "Total area:  " << totalArea << std::endl;
+        return;
+    }
+    // else{
+    //     std::cout << "Total area is " << totalArea << ", drawing triangle" << std::endl;
+    //     std::cout << "Colors: " << (int) color.raw[0] << ", " << (int) color.raw[1] << ", " << (int) color.raw[2] << ", " << (int) color.raw[3] << std::endl;
+    // }
+    //sortTriangleVerticesByX(vertices);
+    std::vector<Vec2i> bbox = computeBoundingBox<Vec3i>(vertices);
+    //std::cout << "bbox min x: " << bbox[0].x << " " << "bbox min y: " << bbox[0].y << " " << "bbox max x: " << bbox[1].x << " " << "bbox max y: " << bbox[1].y << std::endl;
+
+    for (int i=bbox[0].y; i<=bbox[1].y; i++)
+    {
+        for (int j=bbox[0].x; j<=bbox[1].x; j++)
+        {
+            auto result = pointInTriangleBarycentricMethodWithDepthInterpolation(j,i, vertices, totalArea); // returns depth normalized between [0,255]
+            //std::cout << "result: " << *result << std::endl;
+            if (result)
+            {
+                //std::cout << "point in triangle at " << i << " " << j << std::endl;
+                //float depth = ()
+                if (*result > depthBuffer.get(j, i).r)
+                {
+                    //std::cout << "closer pixel detected" << std::endl;
+                    framebuffer.set(j, i, color);
+                    depthBuffer.set(j, i, TGAColor{(int)(*result), (int)(*result), (int)(*result), 255});
+                }
+
+            }
+        }
+    }
+}
+
 std::pair<int, int> convertVec3fToXY(Vec3f &&v, int width, int height)
 {
     return std::make_pair((v.x+1.)*width/2., (v.y+1.)*height/2.);
+}
+
+Vec3i convertVec3fToVec3i(Vec3f v, int width, int height)
+{
+    std::cout << "values in converter: " << v.x << " " << v.y << " " << v.z << std::endl;
+    std::cout << "width: " << width << " height: " << height << std::endl;
+    std::cout << (v.x+1.)*width/2. << " " << (v.y+1.)*height/2. << " " << (v.z+1.)*255/2. << std::endl;
+    return Vec3i{(v.x+1.)*width/2., (v.y+1.)*height/2., (v.z+1.)*255/2.};
+}
+
+std::vector<Vec3i> transformVertices(std::vector<Vec3f> v, int width, int height)
+{
+    std::vector<Vec3i> result;
+    for (int i = 0; i < v.size(); i++)
+    {
+        result.push_back(convertVec3fToVec3i(v[i],width,height));
+    }
+    return result;
 }
 
 
@@ -405,28 +513,43 @@ int main(int argc, char **argv)
         model = new Model("obj/diablo3_pose.obj");
     }
 
-    model->sortFaces();
-    for (int i=0; i<model->nfaces(); i++)
-    {
-        std::cout << "min Z coord: " << std::min(model->vert(model->face(i)[0]).z, std::min(model->vert(model->face(i)[1]).z, model->vert(model->face(i)[2]).z));
-    }
+    // model->sortFaces();
+    // for (int i=0; i<model->nfaces(); i++)
+    // {
+    //     std::cout << "min Z coord: " << std::min(model->vert(model->face(i)[0]).z, std::min(model->vert(model->face(i)[1]).z, model->vert(model->face(i)[2]).z));
+    // }
 
-    TGAImage image(800, 800, TGAImage::RGB);
+    int width = 800;
+    int height = 800;
+
+    TGAImage image(width, height, TGAImage::RGB);
+    TGAImage depthBuffer(width, height, TGAImage::GRAYSCALE);
     std::srand(std::time({}));
 
     for (int i=0; i<model->nfaces(); i++)
     {
         std::vector<int> face = model->face(i);
-        auto [ax, ay] = convertVec3fToXY(model->vert(face[0]), width, height);
-        auto [bx, by] = convertVec3fToXY(model->vert(face[1]), width, height);
-        auto [cx, cy] = convertVec3fToXY(model->vert(face[2]), width, height);
+        std::vector<Vec3f> verticesf{model->vert(face[0]), model->vert(face[1]), model->vert(face[2])};
+        std::vector<Vec3i> vertices = transformVertices(verticesf, width, height);
+        // std::transform(verticesf.begin(), verticesf.end(),
+        //            std::back_inserter(vertices),
+        //            [&width, &height](Vec3f element) {
+        //                return convertVec3fToVec3i(element, width, height);
+        //            });
+        // auto [ax, ay] = convertVec3fToXY(model->vert(face[0]), width, height);
+        // auto [bx, by] = convertVec3fToXY(model->vert(face[1]), width, height);
+        // auto [cx, cy] = convertVec3fToXY(model->vert(face[2]), width, height);
         // TGAColor rnd;
         // for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
+        std::cout << vertices[0].x << " " << vertices[0].y << " " << vertices[0].z << std::endl;
+        std::cout << verticesf[0].x << " " << verticesf[0].y << " " << verticesf[0].z << std::endl;
         const TGAColor randColor = {std::rand()%255, std::rand()%255, std::rand()%255, std::rand()%255};
-        triangleWithFillBoundingBoxMethod(ax, ay, bx, by, cx, cy, image, randColor);
+        triangleWithFillPerPixelPainters(vertices, image, depthBuffer, randColor);
     }
     image.flip_vertically();
-    image.write_tga_file("model_painters_alg.tga");
+    image.write_tga_file("model_per_pixel_painters_alg.tga");
+    depthBuffer.flip_vertically();
+    depthBuffer.write_tga_file("depth_buffer_per_pixel_painters_alg.tga");
     return 0;
 
     // TGAImage framebuffer(width, height, TGAImage::RGB);
